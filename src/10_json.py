b7f3026b2d0844c2f71aba9b87a41c68f17c56a5
Александр Халиков <sashakhalikov@MacBook-Air-Aleksandr.local>
2022-06-03 11:09:42 +0300

    changes to 10_json. working with files

--- src/10_json.py
+++ src/10_json.py
@@ -8,3 +8,13 @@ json_object = json.loads(json_data)
 json_formatted_str = json.dumps(json_object, indent=2)
 
 print(json_formatted_str)
+
+# Working with files
+with open('Cars.json', 'r') as json_file:
+    json_object = json.load(json_file)
+
+print(json_object)
+
+print(json.dumps(json_object))
+
+print(json.dumps(json_object, indent=1))
