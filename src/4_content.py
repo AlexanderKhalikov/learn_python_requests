8c7ac4f0f05d8075987852789610445115fce594
Александр Халиков <sashakhalikov@MacBook-Air-Aleksandr.local>
2022-06-03 10:54:07 +0300

    pprint to 4_content.py and adding 10_json.py

--- src/4_content.py
+++ src/4_content.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 
 response = requests.get('https://api.github.com')
 print('response in bytes:\n\n', response.content)
@@ -7,4 +8,5 @@ print('\n\nresponse as text (unicode):\n\n', response.text)
 # optional
 response.encoding = 'utf-8'
 print('\n\nresponse as text in UTF8:\n\n', response.text)
+pprint(response.json())
 print('\n\nresponse as json:\n\n', response.json())
