911d77ca0209dd0ade3c26f4fdde509410bdfb80
Александр Халиков <sashakhalikov@MacBook-Air-Aleksandr.local>
2022-06-09 20:58:23 +0300

    json updates

--- src/10_json.py
+++ src/10_json.py
@@ -7,14 +7,16 @@ json_object = json.loads(json_data)
 
 json_formatted_str = json.dumps(json_object, indent=2)
 
-print(json_formatted_str)
+# print(json_formatted_str)
 
 # Working with files
 with open('Cars.json', 'r') as json_file:
     json_object = json.load(json_file)
 
-print(json_object)
+print(type(json_object))
+print(f'\nJson objet:\n\n{json_object}')
 
-print(json.dumps(json_object))
+print(type(json.dumps(json_object)))
+print(f'\nJson dumps:\n\n {json.dumps(json_object)}')
 
-print(json.dumps(json_object, indent=1))
+print(f'\nJson dunmps with indent 1:\n\n {json.dumps(json_object, indent=6)}')
